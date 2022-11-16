9045c8db2ff3af98d90219c9e1260e3c8b16a00a
Julia <jul.zakharova@gmail.com>
2022-11-16 08:04:47 +0700

    first commit

---
+++ src/Tank.java
@@ -0,0 +1,14 @@
+public class Tank {
+    private int currentPosition;
+
+    public void goForward(int distance)
+    {this.currentPosition = currentPosition + distance; }
+
+    public void goBackward(int distance)
+    {this.currentPosition = currentPosition - distance; }
+
+    public void printPosition(){
+        System.out.println("The tank is " + currentPosition + " now");
+    }
+
+}
